ff56ca02394e0cf8baf84d80c2e3d16112c6b6c9
kritikam123 <kritikam619@gmail.com>
2026-08-07 09:01:26 +0545

    add product detail functionality

--- global/js/products/products.js
+++ global/js/products/products.js
@@ -1,10 +1,12 @@
 document.addEventListener("productsLoaded", function () {
-    const container = document.getElementById("products-container");
+  const container = document.getElementById("products-container");
 
-    const bestSellers = products.filter(product => product.category === "Home Product");
+  const bestSellers = products.filter(
+    (product) => product.category === "Home Product",
+  );
 
-    bestSellers.forEach(product => {
-        container.innerHTML += `
+  bestSellers.forEach((product) => {
+    container.innerHTML += `
             <div class="card" data-id="${product.id}">
                 <img src="${product.image}">
                 <h3 class="product-info-details" data-id="${product.id}">${product.name}</h3>
@@ -15,23 +17,23 @@ document.addEventListener("productsLoaded", function () {
                 </div>
             </div>
         `;
+  });
+  if (typeof cart !== "undefined") {
+    cart.forEach((item) => {
+      if (typeof markAsAdded === "function") markAsAdded(item.id);
     });
-   if (typeof cart !== "undefined") {
-        cart.forEach(item => {
-            if (typeof markAsAdded === "function") markAsAdded(item.id);
-        });
+  }
+
+  container.addEventListener("click", function (e) {
+    if (e.target.classList.contains("add-btn") && !e.target.disabled) {
+      const id = Number(e.target.dataset.id);
+      if (typeof addToCart === "function") addToCart(id);
+      return;
     }
 
-    container.addEventListener("click", function (e) {
-        if (e.target.classList.contains("add-btn") && !e.target.disabled) {
-            const id = Number(e.target.dataset.id);
-            if (typeof addToCart === "function") addToCart(id);
-            return;
-        }
-
-        if (e.target.classList.contains("product-info-details")) {
-            const id = e.target.dataset.id;
-            window.location.href = `/pages/details/product-details.html?id=${id}`;
-        }
-    });
+    if (e.target.classList.contains("product-info-details")) {
+      const id = e.target.dataset.id;
+      window.location.href = `/pages/details/product-details.html?id=${id}`;
+    }
+  });
 });
