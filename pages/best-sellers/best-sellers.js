f599df0c783a5d5333048e635d692a5b629f79c0
kritikam123 <kritikam619@gmail.com>
2026-08-07 09:05:44 +0545

    Add global cart and profile icon feature

--- pages/best-sellers/best-sellers.js
+++ pages/best-sellers/best-sellers.js
@@ -11,15 +11,16 @@ document.addEventListener("productsLoaded", function () {
             <div class="card ${outOfStock ? "out-of-stock" : ""}" data-id="${product.id}">
                 <img src="${product.image}">
                 <h3 class="product-info-details" data-id="${product.id}">${product.name}</h3>
+                <p>${product.description}</p>
                 <h4>रु.${product.price}</h4>
                 <div class="action-area" id="action-area-${product.id}">
                     <button 
-                        class="add-btn" 
-                        data-id="${product.id}"
-                        ${outOfStock ? "disabled" : ""}
-                    >
-                        ${outOfStock ? "Out of stock" : "Add to cart"}
-                    </button>
+    class="add-btn ${outOfStock ? "out-of-stock" : "available"}"
+    data-id="${product.id}"
+    ${outOfStock ? "disabled" : ""}
+>
+    ${outOfStock ? "Out of stock" : "Add to cart"}
+</button>
                 </div>
             </div>
         `;
